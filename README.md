9b70b22dff9736a1be7a0f1180ba551f13bcc6e5
jomondo <joanmodaw@gmail.com>
2019-01-05 12:58:59 +0100

    show the last 5 products seen in details + add content readme

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
 # Proyect Laravel 5 & Angular 7
 
 ## Functionalities
-+ **List**
-+ **List-Details**
++ **List/Details**
 + **Favourites button**
-
++ **Buy products and pay with stripe**
++ **See the tracking of my products visited**
 
 ## Improvements
 + **Pagination Component**
     - This component can be reused, just need two values and return the current page number to work in other components
++ **Filter Component**
+    - This component can be reused to filter products in other component
 + **Redux**
     - This functionality is important when a app go to be big.
+    - Favourites
+    - Total price in cart
 + **GhaphQl-Apollo**
     - We use this tool to connect frontend and backend.
     - We use queries inside differens places.
     - We use mutations to update a user's favorites-devices list.
+    - From laravel we can see the graphql-ui web interface to show the data
 + **Input**    
 + **Output**    
 + **SASS-CSS-Pre processor**    
-+ **Filters**    
+    - Flexbox
+    - root variables
++ **Filters**
++ **Show the last 5 products seen in Details**
++ **Cart**
+    - We can buy the products, where are you going to compile in localStorage
+    - There is an example of use with redux to show the total price
++ **Stripe**
+    - It is used to pay with a card in the cart
+
++ **Observations**
+    - At first we used angular 5.2.9, but due to a compatibility problem with apollo-graphql we had to migrate the project to angular 7
+    - Previous project with angle 5.2.9 -> https://github.com/joanDeveloper/Laravel_Angular6.git
--- frontend/src/app/device/device.component.ts
+++ frontend/src/app/device/device.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Device, DevicesService, CartService } from '../core';
-//import { ToastrManager } from 'ng6-toastr-notifications';
+import { ActivatedRoute } from '@angular/router';
+import { Device, DevicesService, CartService, UserService, User } from '../core';
 
 @Component({
   selector: 'app-device-page',
-  templateUrl: './device.component.html'
+  templateUrl: './device.component.html',
+  styleUrls: ["./device.component.sass"]
 })
 
 export class DeviceComponent implements OnInit {
@@ -14,19 +14,21 @@ export class DeviceComponent implements OnInit {
   device: { slug: string };
   errors: Object = {};
   isSubmitting = false;
-  
+  currentUser: User;
+  prodVisited: [];
   constructor(
     private cartService: CartService,
     private deviceService: DevicesService,
+    private userService: UserService,
     private route: ActivatedRoute,
-    private router: Router
   ) { }
 
   ngOnInit() {
     this.device = {
       slug: this.route.snapshot.params.slug
     };
-    // Retreive the prefetched article
+    this.getProdVisited();
+    // Retreive the prefetched device
     this.route.data.subscribe(
       (data: { devices: Device[] }) => {
         this.devices = data.devices;
@@ -37,6 +39,7 @@ export class DeviceComponent implements OnInit {
             data => {
               console.log("res frontend devices:", data);
               this.devices = [data];
+              this.setProdVisited(data);
             },
             err => {
               console.log("Error devices", err);
@@ -47,8 +50,32 @@ export class DeviceComponent implements OnInit {
     );
   }
 
-  addCart(Product){
-    console.log("DEVICE PROD",Product);
+  getProdVisited() {
+    this.userService.currentUser.subscribe((userData: User) => { this.currentUser = userData; });
+    let prod = JSON.parse(localStorage.getItem(this.currentUser.username));
+    return this.prodVisited = prod;
+  }
+
+  setProdVisited(prods: Device) {
+    this.userService.currentUser.subscribe((userData: User) => { this.currentUser = userData; });
+    if (localStorage.getItem(this.currentUser.username)) {
+      let array = []; let prod = JSON.parse(localStorage.getItem(this.currentUser.username));
+      array.push(prods, ...prod);
+      // check if the product already exist
+      prod.map(element => {
+        if (element.slug == prods.slug) array.splice(array.indexOf(prods), 1);
+      });
+      // delete last position array (we want show the 5 last products)
+      if(array.length>5) array.pop();
+      
+      localStorage.setItem(this.currentUser.username, JSON.stringify([...array]));
+
+    }else localStorage.setItem(this.currentUser.username, JSON.stringify([prods]));
+    
+  }
+
+  addCart(Product) {
+    console.log("DEVICE PROD", Product);
     this.cartService.addItem(Product);
   }
 }
